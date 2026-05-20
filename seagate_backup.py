7a296288aad55a4019494a6c1f3cd2963750e46f
Guilherme Rouse <radino.comercial@gmail.com>
2026-05-20 09:43:00 -0300

    ajustes seagate_backup.py

--- seagate_backup.py
+++ seagate_backup.py
@@ -8,6 +8,7 @@ from backup import sync_command, seagate_check
 
 SEAGATE_BACKUP_UUID= '6eeeb7f8-1f9d-457e-8b0a-5969c123ea88' # Specify the UUID of your drive
 
+
 # path base para o backup
 origem_base = Path("/","home", "rouse")
 destino_backup = Path("/","media", "seagate_backup")
@@ -23,7 +24,10 @@ def is_mounted(mount_path):
 
 def mount_drive(uuid, mount_path):
     """Mount the drive using its UUID if it's not already mounted."""
+    
+    subprocess.run(["sudo", "mkdir", "-p", str(mount_path)], check=True)
     subprocess.run(["sudo", "mount", f"UUID={SEAGATE_BACKUP_UUID}", mount_path], check=True)
+
 
 def unmount_drive(mount_path):
     """Unmount the drive."""
