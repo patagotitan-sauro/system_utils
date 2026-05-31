eb9b809dee7c8f8e98722171b5d4137343f85121
Guilherme Rouse <radino.comercial@gmail.com>
2026-05-31 11:21:48 -0300

    ajuste rclone onedrive backup

--- rclone_onedriveBackup.sh
+++ rclone_onedriveBackup.sh
@@ -1,14 +1,15 @@
 #!/bin/bash
 
-echo "Backup to OneDrive started at: $(date +"%Y-%m-%d %H:%M:%S")"
-
-#!/bin/bash
+#echo "Backup to OneDrive started at: $(date +"%Y-%m-%d %H:%M:%S")"
 
 /usr/bin/rclone sync $HOME/Documents OneDrive:Linux_Documents \
     --log-file=$HOME/rclone-backup.log \
     --log-level INFO \
-    -v
 
-echo "***"
-echo "Fase 3: Sincronização para a nuvem feita."
-echo "***"
+#Atenção revisar o folder do Onedrive antes de por o script em produção!!!!
+/usr/bin/rclone sync $HOME/Pictures OneDrive:Linux_Pictures \
+    --log-file=$HOME/rclone-backup.log \
+    --log-level INFO \
+#echo "***"
+#echo "Sincronização para a nuvem feita."
+#echo "***"
